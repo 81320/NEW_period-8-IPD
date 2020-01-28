64e8a9d4855d2c8cdc23de60f5a4990894272c16
Dharamavtar Khullar <81320@GEHRHSD.LOCAL>
2020-01-28 13:05:00 -0500

    Update team1.py

--- team1.py
+++ team1.py
@@ -6,6 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
+'''DA'''
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
